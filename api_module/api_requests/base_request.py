4606f09db7520c7e0e7b7ccf97f4ddd83b376ddd
Alexandr Konukhov <jesusmisha@gmail.com>
2023-05-22 13:42:24 +0300

    added base requests

---
+++ api_module/api_requests/base_request.py
@@ -0,0 +1,25 @@
+from ..api_expected_results import BaseRequestResultsExpected as ebr
+
+
+class BaseRequest:
+    def __init__(self, method_api, url='https://reqres.in/api/', **kwargs):
+        self.method_api = method_api
+        self.url = url
+        self.response = None
+        self.params = {key: value for key, value in kwargs.items()}
+
+    def should_be_status_code_200(self):
+        assert self.response.status_code == ebr.STATUS_CODE, \
+            f'Response status code ok with method {self.method_api} should be {ebr.STATUS_CODE},' \
+            f' but got {self.response.status_code}'
+
+    def should_be_result_of_request_in_response(self):
+        assert self.is_result_of_request_in_response() is True, \
+            'Should be result of request but got nothing'
+
+    def is_result_of_request_in_response(self):
+        try:
+            self.response.json()['result']
+        except KeyError:
+            return False
+        return True
---
+++ api_module/api_requests/base_request_get.py
@@ -0,0 +1,9 @@
+import json
+import requests
+from .base_request import BaseRequest
+
+
+class BaseRequestGet(BaseRequest):
+    def send(self):
+        self.response = requests.get(self.url + self.method_api)
+        return self.response
