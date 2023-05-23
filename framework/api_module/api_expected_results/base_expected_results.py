c7906cfb15865009ab5e575434b1d0cd8c1028d6
Alexander Konukhov <jesusmisha@gmail.com>
2023-05-23 21:37:43 +0300

    added STATUS_CODE_MISSING to expected results

--- framework/api_module/api_expected_results/base_expected_results.py
+++ framework/api_module/api_expected_results/base_expected_results.py
@@ -7,6 +7,7 @@ class BaseRequestResultsExpected:
     """Class for Base Request expected results."""
     STATUS_CODE = 200
     STATUS_CODE_NOT_FOUND = 404
+    STATUS_CODE_MISSING = 400
     STATUS_CODE_CREATED = 201
     SUPPORT_URL = 'https://reqres.in/#support-heading'
     SUPPORT_TEXT = 'To keep ReqRes free, contributions towards server costs are appreciated!'
