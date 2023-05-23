5fe526f2900acdc9a42119d642337ee29aae6e1a
Alexander Konukhov <jesusmisha@gmail.com>
2023-05-23 21:38:59 +0300

    added register_unsuccessful api tests

--- framework/api_module/api_tests/test_post_register_successful.py
+++ framework/api_module/api_tests/test_post_register_successful.py
@@ -1,5 +1,5 @@
 """
-API tests for RegisterSuccessful method.
+API tests for RegisterSuccessful and RegisterUnsuccessful method.
 """
 import json
 import pytest
@@ -9,9 +9,15 @@ from .. import generated_person
 
 
 @allure.step('Get response body for ui test')
-def get_received_response_body_for_ui_register_successful(password_to_send, email_to_send) -> dict:
-    request = RegisterSuccessful('register', password='', email='')
-    request.send_request_with_email_and_password(request, password_to_send, email_to_send)
+def get_received_response_body_for_ui_register_successful(
+        password_to_send, email_to_send, type_of_registration='success') -> dict:
+    if type_of_registration == 'success':
+        request = RegisterSuccessful('register', password='', email='')
+        request.send_request_with_email_and_password(request, password_to_send, email_to_send)
+    elif type_of_registration == 'unsuccessful':
+        request = RegisterSuccessful('register', email='')
+        request.params['email'] = email_to_send
+        request.send()
     return request.get_response()
 
 
@@ -46,3 +52,26 @@ class TestPostRegisterSuccessful:
         password_to_send = person.password
         request.send_request_with_email_and_password(request, password_to_send)
         request.check_id_and_token_in_response()
+
+    @allure.title('Test unsuccesseful registeration with missing email.')
+    def test_post_register_unsuccessful_should_return_status_code_400(self):
+        request = RegisterSuccessful('register', password='')
+        person = next(generated_person())
+        password_to_send = person.password
+        request.send_request_with_password_only(request, password_to_send)
+        request.should_be_status_code_400()
+
+    @allure.title('Test unsuccesseful registeration with missing password.')
+    def test_post_register_unsuccessful_should_return_status_code_400(self):
+        request = RegisterSuccessful('register', email='')
+        email_to_send = 'sydney@fife'
+        request.send_request_with_email_only(request, email_to_send)
+        request.should_be_status_code_400()
+
+    @allure.title('Test unsuccesseful registeration with incorrect email.')
+    def test_post_register_unsuccessful_should_return_status_code_400(self):
+        person = next(generated_person())
+        password_to_send = person.password
+        email_to_send = person.email
+        request.send_request_with_email_and_password(request, password_to_send, email_to_send)
+        request.should_be_status_code_400()
