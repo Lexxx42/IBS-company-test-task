b9dd579906a559301e7ea09df03a34a04f64f99a
Alexandr Konukhov <jesusmisha@gmail.com>
2023-05-23 09:52:30 +0300

    added tests for single user

--- framework/web_module/web_tests/test_main_page.py
+++ framework/web_module/web_tests/test_main_page.py
@@ -78,14 +78,14 @@ class TestMainPage:
             """Test user can view status code of API call."""
             main_page = MainPage(driver, self.main_page_link)
             main_page.open()
-            main_page.check_ui_status_code_output()
+            main_page.check_ui_status_code_output_list_users()
 
         @allure.title('List Users request shows correct request URL in main page.')
         def test_request_url_ui_output(self, driver) -> None:
             """Test user can view request url of API call."""
             main_page = MainPage(driver, self.main_page_link)
             main_page.open()
-            main_page.check_request_url_output()
+            main_page.check_request_url_output_list_users()
 
         @allure.title('List Users request shows correct result in main page.')
         def test_list_users_request_shows_correct_result(self, driver) -> None:
@@ -93,3 +93,29 @@ class TestMainPage:
             main_page = MainPage(driver, self.main_page_link)
             main_page.open()
             main_page.check_correct_list_users_request()
+
+    @allure.feature('Single User request')
+    class TestSingleUser:
+        """Class represents Single User request tests."""
+        main_page_link = 'https://reqres.in/'
+
+        @allure.title('Single User request shows correct status code in main page.')
+        def test_status_code_ui_output(self, driver) -> None:
+            """Test user can view status code of API call."""
+            main_page = MainPage(driver, self.main_page_link)
+            main_page.open()
+            main_page.check_ui_status_code_output_single_user()
+
+        @allure.title('List Users request shows correct request URL in main page.')
+        def test_request_url_ui_output(self, driver) -> None:
+            """Test user can view request url of API call."""
+            main_page = MainPage(driver, self.main_page_link)
+            main_page.open()
+            main_page.check_request_url_output_single_user()
+
+        @allure.title('List Users request shows correct result in main page.')
+        def test_list_users_request_shows_correct_result(self, driver) -> None:
+            """Test user can fill the form and sent it."""
+            main_page = MainPage(driver, self.main_page_link)
+            main_page.open()
+            main_page.check_correct_single_user_request()
