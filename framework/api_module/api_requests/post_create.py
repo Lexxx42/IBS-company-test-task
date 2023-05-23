bc860a1d84d8044c499eca7176d9fd6d56374a53
Alexander Konukhov <jesusmisha@gmail.com>
2023-05-23 19:16:14 +0300

    added post create api methods

--- framework/api_module/api_requests/post_create.py
+++ framework/api_module/api_requests/post_create.py
@@ -6,14 +6,26 @@ from .base_request_post import BaseRequestPost
 class PostCreate(BaseRequestPost):
 
     @allure.step('Send request with user\'s name and job.')
-    def send_request_with_name_and_job(self, request, name: str, job: str) -> None:
-        request.method_api = self.method_api
+    def send_request_with_name_and_job(self, request, name_to_send, job_to_send) -> tuple[str, str]:
+        request.params['name'] = name_to_send
+        request.params['job'] = job_to_send
         request.send()
+        return name_to_send, job_to_send
 
     @allure.step('Get requests response.')
     def get_response(self) -> dict:
         response = self.response.json()
         return response
+
+    @allure.step('Check response to have sent name and job.')
+    def check_job_and_name_in_response(self, sent_name, sent_job):
+        response = self.get_response()
+        assert response['name'] == sent_name, \
+            f'Expected response to have {sent_name=}.' \
+            f'\n Got: {response["name"]}'
+        assert response['job'] == sent_job, \
+            f'Expected response to have {sent_job=}.' \
+            f'\n Got: {response["job"]}'
 
     @allure.step('Check id user id in response.')
     def should_be_correct_user_id(self, user_id_request: int) -> None:
