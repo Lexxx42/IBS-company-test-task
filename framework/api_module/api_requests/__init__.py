842c92e7c94c282efe2a38c4af9b7bfad4374970
Alexandr Konukhov <jesusmisha@gmail.com>
2023-05-23 16:50:56 +0300

    added imports

--- framework/api_module/api_requests/__init__.py
+++ framework/api_module/api_requests/__init__.py
@@ -3,3 +3,4 @@ from .get_single_user import GetSingleUser
 from .get_list_resource import GetListResource
 from .get_single_resource import GetSingleResource
 from .get_delayed_response import GetDelayedResponse
+from .post_create import PostCreate
--- framework/api_module/api_tests/__init__.py
+++ framework/api_module/api_tests/__init__.py
@@ -1,3 +1,4 @@
+from .. import generated_person
 from .test_get_list_users import TestGetListUsersMethod
 from .test_get_list_users import get_total_users, get_received_response_body_for_ui_list_users, \
     get_request_url_for_ui_list_users
