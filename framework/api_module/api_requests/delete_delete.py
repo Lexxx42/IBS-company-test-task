efff6cb0a5f9715f05c277c4fabbfca1030212d1
Alexandr Konukhov <jesusmisha@gmail.com>
2023-05-24 12:36:21 +0300

    deleted unused methods

--- framework/api_module/api_requests/delete_delete.py
+++ framework/api_module/api_requests/delete_delete.py
@@ -8,17 +8,3 @@ class DeleteDelete(BaseRequestDelete):
     def send_request_with_selected_user_id(self, request, user_id: int) -> None:
         request.method_api = self.method_api + str(user_id)
         request.send()
-
-    @allure.step('Check id user id in response.')
-    def should_be_correct_user_id(self, user_id_request: int) -> None:
-        user_id_response = self.get_response()['data']['id']
-        assert user_id_request == user_id_response, \
-            f'User id in request: {user_id_request},\n' \
-            f' should be equal to user id in response: {user_id_response}'
-
-    @allure.step('Check data with incorrect user id.')
-    def should_not_be_data_with_id(self) -> None:
-        data = self.response.json()
-        number_of_data_items = len(data.items())
-        assert number_of_data_items == 0, \
-            f'Should be no data. But got: {data}'
