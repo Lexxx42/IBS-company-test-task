dc673df8361bfba1ab8295aacb8be0ef6a2d7010
Alexandr Konukhov <jesusmisha@gmail.com>
2023-05-24 09:50:14 +0300

    added base request put

---
+++ framework/api_module/api_requests/base_request_put.py
@@ -0,0 +1,10 @@
+import json
+import requests
+from .base_request import BaseRequest
+
+
+class BaseRequestPut(BaseRequest):
+    def send(self):
+        self.response = requests.put(self.url + self.method_api,
+                                     data=self.params)
+        return self.response
