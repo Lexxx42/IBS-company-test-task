7ae1530890b7775fa7d6d1d03a157975c997828e
Alexandr Konukhov <jesusmisha@gmail.com>
2023-05-24 10:24:54 +0300

    added base patch request

---
+++ framework/api_module/api_requests/base_request_patch.py
@@ -0,0 +1,10 @@
+import json
+import requests
+from .base_request import BaseRequest
+
+
+class BaseRequestPatch(BaseRequest):
+    def send(self):
+        self.response = requests.patch(self.url + self.method_api,
+                                       data=self.params)
+        return self.response
